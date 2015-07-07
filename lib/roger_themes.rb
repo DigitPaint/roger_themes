e337c184fa497c76963e2cb9985e6386401de74b
Edwin van der Graaf <edwin@digitpaint.nl>
2015-07-07 11:25:07 +0200

    Load middleware, processor and finalizers from lib root

--- lib/roger_themes.rb
+++ lib/roger_themes.rb
@@ -3,3 +3,7 @@ require "roger_themes/version"
 module RogerThemes
   # Your code goes here...
 end
+
+require "roger_themes/middleware"
+require "roger_themes/processor"
+require "roger_themes/xc_finalizer"
