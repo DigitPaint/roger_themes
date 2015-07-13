06bea0894524e988bdc287c45a692744c969cbfd
Edwin van der Graaf <edwin@digitpaint.nl>
2015-07-13 11:17:23 +0200

    Fallback to shared images or fonts if request http status != 200

--- lib/roger_themes/middleware.rb
+++ lib/roger_themes/middleware.rb
@@ -18,32 +18,33 @@ module RogerThemes
       if theme = path[r,1]
         orig_path = env["PATH_INFO"].dup
         env["SITE_THEME"] = theme
-        env["PATH_INFO"].sub!(r,"");
+        env["PATH_INFO"].sub!(r,"")
       else
         # Set default theme
         env["SITE_THEME"] = @options[:default_theme]
       end
 
-      # BIG TODO -- add guard statements when image is in
-      # theme image folder
-      # Check to if we are a asset i.e. image
-      r = /\A\/themes\/([^\/]+)\/rel\/images\//
-      if asset = path[r, 1]
-        orig_path = env["PATH_INFO"].dup
-        env["PATH_INFO"].sub!(r,"/images/");
-      end
-
-      r = /\A\/themes\/([^\/]+)\/rel\/fonts\//
-      if asset = path[r, 1]
-        orig_path = env["PATH_INFO"].dup
-        env["PATH_INFO"].sub!(r,"/fonts/");
-      end
-
-
       ret = @app.call(env)
 
-      # Restore path so we are nice with everybody else
-      env["PATH_INFO"] = orig_path
+      # Fallback for shared images
+      unless ret[0] == 200
+        r = /\A\/themes\/([^\/]+)\/rel\/images\//
+        if asset = path[r, 1]
+          orig_path = env["PATH_INFO"].dup
+          env["PATH_INFO"].sub!(r, "/images/")
+        end
+
+        r = /\A\/themes\/([^\/]+)\/rel\/fonts\//
+        if asset = path[r, 1]
+          orig_path = env["PATH_INFO"].dup
+          env["PATH_INFO"].sub!(r, "/fonts/")
+        end
+
+        ret = @app.call(env)
+
+        # Restore path so we are nice with everybody else
+        env["PATH_INFO"] = orig_path
+      end
 
       ret
     end
