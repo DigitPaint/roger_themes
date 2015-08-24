799be5fb596d392aaa366032809532176662968e
Flurin Egger <flurin@digitpaint.nl>
2015-08-24 11:40:39 +0200

    Parameterize xc_finalizer zip/source paths

--- lib/roger_themes/xc_finalizer.rb
+++ lib/roger_themes/xc_finalizer.rb
@@ -4,17 +4,27 @@ module RogerThemes
   class XcFinalizer < Roger::Release::Finalizers::Base
     attr_reader :release
 
+    # XC finalizer finalizes designzips.
+    #
+    # @param [Release] release
+    # @param [Hash] options Options hash
+    # @option options [String] :prefix ("html") The name to prefix the zipfile with (before version)
+    # @option options [String] :zip ("zip") The ZIP command to use
+    # @option options [String] :source_path ("themes/*") The paths to zip
+    # @option options [String] :target_path ("themes/zips") The path to the zips
     def call(release, options = {})
-      dirs = Dir.glob((release.build_path + "themes/*").to_s)
-
       options = {
         :prefix => "html",
-        :zip => "zip"
+        :zip => "zip",
+        :source_path => "themes/*",
+        :target_path => "themes/zips"
       }.update(options)
+
+      dirs = Dir.glob((release.build_path + options[:source_path]).to_s)
 
       releasename = [(options[:prefix] || "html"), release.scm.version].join("-")
 
-      zipdir = release.build_path + "themes/zips"
+      zipdir = release.build_path + options[:target_path]
       FileUtils.mkdir_p(zipdir) unless zipdir.exist?
 
       dirs.each do |dir|
