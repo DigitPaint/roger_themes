f4d021576bb3c9fdbc90594b73ed292ee28389e2
Flurin Egger <flurin@digitpaint.nl>
2015-08-24 12:11:46 +0200

    Allow setting of full paths in target_path/source_path

--- lib/roger_themes/xc_finalizer.rb
+++ lib/roger_themes/xc_finalizer.rb
@@ -10,21 +10,21 @@ module RogerThemes
     # @param [Hash] options Options hash
     # @option options [String] :prefix ("html") The name to prefix the zipfile with (before version)
     # @option options [String] :zip ("zip") The ZIP command to use
-    # @option options [String] :source_path ("themes/*") The paths to zip
-    # @option options [String] :target_path ("themes/zips") The path to the zips
+    # @option options [String] :source_path (release.build_path + "themes/*") The paths to zip
+    # @option options [String] :target_path (release.build_path + "themes/zips") The path to the zips
     def call(release, options = {})
       options = {
         :prefix => "html",
         :zip => "zip",
-        :source_path => "themes/*",
-        :target_path => "themes/zips"
+        :source_path => release.build_path + "themes/*",
+        :target_path => release.build_path + "themes/zips"
       }.update(options)
 
-      dirs = Dir.glob((release.build_path + options[:source_path]).to_s)
+      dirs = Dir.glob(options[:source_path].to_s)
 
       releasename = [(options[:prefix] || "html"), release.scm.version].join("-")
 
-      zipdir = release.build_path + options[:target_path]
+      zipdir = options[:target_path]
       FileUtils.mkdir_p(zipdir) unless zipdir.exist?
 
       dirs.each do |dir|
